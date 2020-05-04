6b4850b265a58f33e0830a3980f6dae00081028b
sserdyuk <sserdyuk@fun.co>
2020-05-04 15:27:19 +0300

    fix

--- src/ru/serdyuk/tester/MainTester.java
+++ src/ru/serdyuk/tester/MainTester.java
@@ -1,14 +1,15 @@
 package ru.serdyuk.tester;
 
 import ru.serdyuk.tester.task.LuckyTicketTask;
+import ru.serdyuk.tester.task.StringLengthTask;
 import ru.serdyuk.tester.task.Task;
 
 public class MainTester {
 
     public static void main(String[] args) {
-//        Task task = new StringLength();
-//        Tester tester0 = new Tester(task, "resources/0.String");
-//        tester0.runTests();
+        Task task = new StringLengthTask();
+        Tester tester0 = new Tester(task, "resources/0.String");
+        tester0.runTests();
 
         Tester tester1 = new Tester(new LuckyTicketTask(), "resources/1.Tickets");
         tester1.runTests();
